229b342e4f885d0873f36627b0ff976b4e19d341
salsa-bf <khnnpubg5@gmail.com>
2025-01-31 17:44:57 +0700

    Add files via upload

---
+++ js/transfer.js
@@ -0,0 +1,5 @@
+var noRek = document.getElementById("norek");
+
+document.getElementById("transfer").onclick = function () {
+  window.location.href = "https://wa.me/+6285802792037?text=" + noRek.value;
+};
